e687bdc513b2d72863a743e146c12c6b630dd851
Osorabrian <osorabrian@gmail.com>
2022-12-07 21:44:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+function reverseString(word){
+  // takes string splits it, reverses it and then joins it together to form a reversed string.
+    return word.split("").reverse().join("")
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  // if the reversed string is the same as the input
+  //   return true
+  // else
+  //   return false
+  //compare reversed string to oroginal string it returns true
+  return reverseString(word) === word
 }
 
 /* 
-  Add your pseudocode here
+  Create a reverseString function that reverses the string.
+
+  create boolean expression to compare original string and reversed string
 */
 
 /*
-  Add written explanation of your solution here
+  The function will take in a string, then split into a list then we reverse the list 
+  after the list has been reversed we join it to form a reversed string. After that we compare
+  if the original string and the reversed string match using boolean expression, if they are equal
+  the function isPlindrome should return true else it return false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +36,34 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome('brian'))
+
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome('mum'))
+
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome(''))
+
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome(" "))
+
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome('a'))
+
+  console.log("Expecting: ih");
+console.log("=>", reverseString("hi"));
+
+console.log("");
+
+console.log("Expecting: tobor");
+console.log("=>", reverseString("robot"));
+
+console.log("");
+
+console.log("Expecting: mom");
+console.log("=>", reverseString("mom"));
 }
 
 module.exports = isPalindrome;
